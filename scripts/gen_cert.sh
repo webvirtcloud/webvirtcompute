e8bb988d4bde9d9674f93ba10447cfe533318c1b
Retspen <anatoliy.guskov@gmail.com>
2023-01-01 18:29:25 +0200

    Updated gen cert script

--- scripts/gen_cert.sh
+++ scripts/gen_cert.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-openssl req -new -x509 -keyout server.pem -out server.pem -days 365 -nodes -subj "/C=EU/ST=Ukraine/L=Zaporozhye/O=hostwebvirt/CN=172.32.16.10"
+openssl req -new -x509 -keyout server.pem -out server.pem -days 365 -nodes -subj "/C=EU/ST=Ukraine/L=Zaporozhye/O=webvirtcloud/CN=172.64.0.2"
 
 exit 0
