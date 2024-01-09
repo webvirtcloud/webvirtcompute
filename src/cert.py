429fbaad1e43111eab0536fe1c7ae8f7f8e631e2
Retspen <anatoliy.guskov@gmail.com>
2024-01-09 11:40:40 +0200

    Added cleanup certs folder

--- src/cert.py
+++ src/cert.py
@@ -11,8 +11,9 @@ def gen_self_signed_cert(validity_days=3650):
     Generate self-signed certificate and private key and save them to files. Default validity is 10 years.
     """
     generated_hex = secrets.token_hex(8)
-    key_file_path = os.path.join(TMPDIR, f"key-{generated_hex}.pem")
-    cert_file_path = os.path.join(TMPDIR, f"cert-{generated_hex}.pem")
+    tmp_cert_folder = os.path.join(TMPDIR, generated_hex)
+    key_file_path = os.path.join(tmp_cert_folder, "key.pem")
+    cert_file_path = os.path.join(tmp_cert_folder, "cert.pem")
 
     key = crypto.PKey()
     key.generate_key(crypto.TYPE_RSA, 4096)
@@ -22,7 +23,6 @@ def gen_self_signed_cert(validity_days=3650):
     cert.get_subject().ST = "California"
     cert.get_subject().L = "San Francisco"
     cert.get_subject().O = "WebVirtCloud"
-    cert.get_subject().OU = "WebVirtCloud"
     cert.get_subject().CN = "localhost"
     cert.get_subject().emailAddress = "admin@webvirt.cloud"
     cert.set_serial_number(0)
@@ -31,6 +31,8 @@ def gen_self_signed_cert(validity_days=3650):
     cert.set_issuer(cert.get_subject())
     cert.set_pubkey(key)
     cert.sign(key, "sha512")
+
+    os.mkdir(tmp_cert_folder)
 
     with open(cert_file_path, "wt") as f:
         f.write(crypto.dump_certificate(crypto.FILETYPE_PEM, cert).decode("utf-8"))
--- src/webvirtcompute.py
+++ src/webvirtcompute.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
 
+import os
+import shutil
 import uvicorn
 from settings import HOST, PORT
 from cert import gen_self_signed_cert
 
 
 if __name__ == "__main__":
-    key_file, cert_file = gen_self_signed_cert()
-    uvicorn.run("main:app", host=HOST, port=PORT, ssl_keyfile=key_file, ssl_certfile=cert_file, log_level="info")
+    try:
+        key_file, cert_file = gen_self_signed_cert()
+        uvicorn.run("main:app", host=HOST, port=PORT, ssl_keyfile=key_file, ssl_certfile=cert_file, log_level="info")
+    finally:
+        shutil.rmtree(os.path.dirname(key_file), ignore_errors=True)
