9b6c73642592ab44c1a9fc7a0a684129dea3fdef
Retspen <anatoliy.guskov@gmail.com>
2024-02-22 15:43:17 +0200

    Updated dev script

--- src/webvirtcomputedev.py
+++ src/webvirtcomputedev.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
 
+import os
+import shutil
 import uvicorn
 from settings import HOST, PORT
+from cert import gen_self_signed_cert
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host=HOST, port=PORT, reload=True, access_log=True)
+    try:
+        key_file, cert_file = gen_self_signed_cert()
+        uvicorn.run("main:app", host=HOST, port=PORT, reload=True, access_log=True)
+    finally:
+        shutil.rmtree(os.path.dirname(key_file), ignore_errors=True)
